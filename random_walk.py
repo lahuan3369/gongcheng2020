0e5d33dafe6c72125349f4ca01475e9cd525e5b3
lahuan3369 <1499703198@qq.com>
2020-08-11 22:00:07 +0800

    fourth commit

--- random_walk.py
+++ random_walk.py
@@ -1,18 +1,22 @@
 from random import choice
 
 class RandomWalk():
+	"""一个生成随机漫步数据的类"""
 	
 	def __init__(self,num_points=5000):
+		"""初始化随机漫步的属性"""
 		self.num_points=num_points
+		
 		self.x_values=[0]
 		self.y_values=[0]
 		
 	def fill_walk(self):
+		"""计算随机漫步包含的所有点"""
 		while len(self.x_values)<self.num_points:
-			
 			x_direction=choice([1,-1])
 			x_distance=choice([0,1,2,3,4,9])
 			x_step=x_direction*x_distance
+			
 			y_direction=choice([1,-1])
 			y_distance=choice([0,1,2,3,4,9])
 			y_step=y_direction*y_distance
--- rw_visual.py
+++ rw_visual.py
@@ -1,14 +1,16 @@
 import matplotlib.pyplot as plt
 
 from random_walk import RandomWalk
+
 while True:
 	rw=RandomWalk(70000)
 	rw.fill_walk()
 	
 	plt.figure(figsize=(10,6))
 	point_numbers=list(range(rw.num_points))
-	plt.scatter(rw.x_values,rw.y_values,c=point_numbers,
-	cmap=plt.cm.Oranges,edgecolor='none',s=1)
+	plt.scatter(rw.x_values,rw.y_values,
+	c=point_numbers,cmap=plt.cm.Oranges,
+	edgecolor='none',s=1)
 	plt.show()
 	
 	keep_running=input("再来一次?(y/n):")
